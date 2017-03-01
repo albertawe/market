9d3d6b583f066c4578f5917e31523c7b84f52f93
awe <albertsagi97@gmail.com>
2017-03-01 11:12:57 +0700

    css and base view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+/* Route Model Binding */
+Route::model('product','App\Product');
+Route::model('laporan','App\Transaction');
+
+/* Route Untuk Autentikasi Admin */
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+/* Route Untuk Pengunjung */
+Route::get('/', 'TokoController@index');
+
+Route::get('/product/{id}/show',['as' => 'product.show','uses' => 'TokoController@showItem']);
+
+Route::get('product/cart/{id}', 'TokoController@tambahItem');
+
+Route::get('cart/delete/{id}' , 'TokoController@hapusItem');
+
+Route::get('cart/checkout', 'TokoController@checkout');
+
+Route::post('/cart/{form_id}/save',['as' => 'pelanggan.save','uses' => 'TokoController@savePelanggan']);
+
+/* Route Untuk Administrator */
+Route::group( [
+    'middleware' => 'auth' ,
+        ] , function() {
+   
+   get('/dashboard', function()
+	{
+		return view('toko.admin.home');
+	}); 
+
+	resource('dashboard/product', 'AdminProductController');
+	get('dashboard/laporan', 'AdminLaporanController@index');
+	post('dashboard/laporan/periode',['as' => 'laporan.show','uses' => 'AdminLaporanController@getPeriode']);
+
+} );
